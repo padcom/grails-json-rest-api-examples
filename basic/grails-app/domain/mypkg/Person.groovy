dc8033634b3c156c9e6f7d227fa92405f5128a95
Matthias Hryniszak <padcom@gmail.com>
2011-05-06 13:17:59 +0200

    Added transient field to Person

--- basic/grails-app/domain/mypkg/Person.groovy
+++ basic/grails-app/domain/mypkg/Person.groovy
@@ -5,4 +5,14 @@ class Person {
   
   String firstName
   String lastName
+
+  static api [
+    transients: [ 'fullName' ]
+  ]
+
+  static transients = [ 'fullName' ]
+
+  String getFullName() {
+    "${firstName} ${lastName}"
+  }
 }
