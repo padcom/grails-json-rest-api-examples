5f6d1b627a4b67f68b948572df0bd93a8b7383f5
Maciej Hryniszak <maciej.hryniszak@travelocity.com>
2011-09-26 06:59:25 +0200

    Enabled SQL logging in order to find out what's queried in relations

--- relations/grails-app/conf/DataSource.groovy
+++ relations/grails-app/conf/DataSource.groovy
@@ -15,6 +15,7 @@ environments {
         dataSource {
             dbCreate = "create-drop" // one of 'create', 'create-drop','update'
             url = "jdbc:hsqldb:file:target/db/dev;shutdown=true"
+			logSql = true
         }
     }
     test {
